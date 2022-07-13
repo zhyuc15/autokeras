ab6943cdf3f8a423b3d8171c01a0ebc691b9f86f
zhyuc15 <malaga540josh@gmail.com>
2022-07-13 12:37:18 +0800

    0713update

--- autokeras0712.py
+++ autokeras0712.py
@@ -1,45 +1,35 @@
+import numpy as np
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.datasets import cifar10
 import matplotlib.pyplot as plt
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
-train_images, test_images = train_images / 255.0, test_images / 255.0
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i])
-    # The CIFAR labels happen to be arrays,
-    # which is why you need the extra index
-    plt.xlabel(class_names[train_labels[i][0]])
+import autokeras as ak
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print(x_train.shape)
+print(x_test.shape)
+fig=plt.figure()
+bin= np.arange(11)
+ax=fig.add_subplot(1,2,1)
+ax.set_xticks(bin)
+plt.hist(y_train,bins=bin-0.5,rwidth=0.9)
+ax.set_title('Train dataset histogram')
+
+ax=fig.add_subplot(1,2,2)
+ax.set_xticks(bin)
+plt.hist(y_train,bins=bin-0.5,rwidth=0.9)
+ax.set_title('Train dataset histogram')
 plt.show()
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
-
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-history = model.fit(train_images, train_labels, epochs=5, 
-                    validation_data=(test_images, test_labels))
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
-print(test_loss)
-print(test_acc)
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
+clf=ak.ImageClassifier(max_trials=3)
+clf.fit(x_train,y_train)
+clf.evaluate(x_test,y_test)
+predicted=clf.predict(x_test[:10])
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+fig=plt.figure(figsize=(16,6))
+for i in range(10):
+    ax=fig.add_subplot(2,5,i+1)
+    ax.set_axis_off()
+    plt.imshow(x_test[i])
+    ax.set_title(f'Predicted:{labels[int(predicted[i])]},Real:{labels[int(y_test[i])]}')
+    plt.tight_layout()
+    plt.show()
+model = clf.export_model()
+model.summary()
